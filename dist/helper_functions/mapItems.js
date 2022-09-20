e6aca488ce8d0b6169197fc238372e854399a73f
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2022-09-20 23:56:48 +0200

    Redundant js files deleted

--- dist/helper_functions/mapItems.js
+++
@@ -1,8 +0,0 @@
-const mapItems = (names, ids) => {
-    const result = [];
-    for (let i = 0; i < names.length; i++) {
-        result.push({ id: ids[i], name: names[i] });
-    }
-    return result;
-};
-export default mapItems;

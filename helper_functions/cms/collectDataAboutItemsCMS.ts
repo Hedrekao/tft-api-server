e70787e85dc3a2dc329723ea56122c930235c615
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2022-09-30 00:40:56 +0200

    Save cms route finished, not matching comps in order to get more data

---
+++ helper_functions/cms/collectDataAboutItemsCMS.ts
@@ -0,0 +1,25 @@
+import { Comp } from 'types/classes';
+
+const collectDataAboutItemsCMS = (
+  composition: Array<Object>,
+  itemsData: Object,
+  compositionUnits: Object,
+  compositionInput: Comp
+) => {
+  for (const unit of compositionInput.units) {
+    if (!itemsData.hasOwnProperty(unit.name)) {
+      itemsData[unit.name] = {};
+    }
+    for (const item of compositionUnits[unit['name']]['items']) {
+      const unitItems = itemsData[unit['name']];
+      if (unitItems.hasOwnProperty(item)) {
+        unitItems[item]['numberOfComps'] += 1;
+      } else {
+        unitItems[item] = {};
+        unitItems[item]['numberOfComps'] = 1;
+      }
+    }
+  }
+};
+
+export default collectDataAboutItemsCMS;

2f2baf033d800c408b8b1299426d853c6c1d3b63
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2022-10-10 14:51:18 +0200

    Routes for first, second, third choices of augment

--- helper_functions/tasks/analyzeAugmentsPerformance.ts
+++ helper_functions/tasks/analyzeAugmentsPerformance.ts
@@ -1,8 +1,75 @@
 const analyzeAugmentsPerformance = (
   augmentsObject: Object,
+  firstChoiceAugmentObject: Object,
+  secondChoiceAugmentObject: Object,
+  thirdChoiceAugmentObject: Object,
   composition: Array<Object>
 ) => {
-  for (const augment of composition['augments']) {
+  for (let i = 0; i < composition['augments'].length; i++) {
+    let augment: string = composition['augments'][i];
+    augment = augment.replace('HR', '');
+    switch (i) {
+      case 0:
+        if (firstChoiceAugmentObject.hasOwnProperty(augment)) {
+          firstChoiceAugmentObject[augment]['sumOfPlacement'] +=
+            composition['placement'];
+          firstChoiceAugmentObject[augment]['frequency'] += 1;
+          if (composition['placement'] == 1) {
+            firstChoiceAugmentObject[augment]['winrate'] += 1;
+          }
+        } else {
+          firstChoiceAugmentObject[augment] = {};
+          firstChoiceAugmentObject[augment]['sumOfPlacement'] =
+            composition['placement'];
+          firstChoiceAugmentObject[augment]['frequency'] = 1;
+          if (composition['placement'] == 1) {
+            firstChoiceAugmentObject[augment]['winrate'] = 1;
+          } else {
+            firstChoiceAugmentObject[augment]['winrate'] = 0;
+          }
+        }
+        break;
+      case 1:
+        if (secondChoiceAugmentObject.hasOwnProperty(augment)) {
+          secondChoiceAugmentObject[augment]['sumOfPlacement'] +=
+            composition['placement'];
+          secondChoiceAugmentObject[augment]['frequency'] += 1;
+          if (composition['placement'] == 1) {
+            secondChoiceAugmentObject[augment]['winrate'] += 1;
+          }
+        } else {
+          secondChoiceAugmentObject[augment] = {};
+          secondChoiceAugmentObject[augment]['sumOfPlacement'] =
+            composition['placement'];
+          secondChoiceAugmentObject[augment]['frequency'] = 1;
+          if (composition['placement'] == 1) {
+            secondChoiceAugmentObject[augment]['winrate'] = 1;
+          } else {
+            secondChoiceAugmentObject[augment]['winrate'] = 0;
+          }
+        }
+        break;
+      case 2:
+        if (thirdChoiceAugmentObject.hasOwnProperty(augment)) {
+          thirdChoiceAugmentObject[augment]['sumOfPlacement'] +=
+            composition['placement'];
+          thirdChoiceAugmentObject[augment]['frequency'] += 1;
+          if (composition['placement'] == 1) {
+            thirdChoiceAugmentObject[augment]['winrate'] += 1;
+          }
+        } else {
+          thirdChoiceAugmentObject[augment] = {};
+          thirdChoiceAugmentObject[augment]['sumOfPlacement'] =
+            composition['placement'];
+          thirdChoiceAugmentObject[augment]['frequency'] = 1;
+          if (composition['placement'] == 1) {
+            thirdChoiceAugmentObject[augment]['winrate'] = 1;
+          } else {
+            thirdChoiceAugmentObject[augment]['winrate'] = 0;
+          }
+        }
+        break;
+    }
     if (augmentsObject.hasOwnProperty(augment)) {
       augmentsObject[augment]['sumOfPlacement'] += composition['placement'];
       augmentsObject[augment]['frequency'] += 1;
--- helper_functions/tasks/calculateAndSaveAugmentsIntoDb.ts
+++ helper_functions/tasks/calculateAndSaveAugmentsIntoDb.ts
@@ -2,7 +2,12 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const calculateAndSaveAugmentsDataIntoDb = async (augmentsObject: Object) => {
+const calculateAndSaveAugmentsDataIntoDb = async (
+  augmentsObject: Object,
+  firstChoiceAugmentObject: Object,
+  secondChoiceAugmentObject: Object,
+  thirdChoiceAugmentObject: Object
+) => {
   try {
     for (const id in augmentsObject) {
       const numOfRecords = await prisma.augments_ranking.count({
@@ -28,6 +33,93 @@ const calculateAndSaveAugmentsDataIntoDb = async (augmentsObject: Object) => {
             sumOfPlacements: augmentsObject[id]['sumOfPlacement'],
             numberOfAppearances: augmentsObject[id]['frequency'],
             sumOfWins: augmentsObject[id]['winrate']
+          }
+        });
+      }
+    }
+
+    for (const id in firstChoiceAugmentObject) {
+      const numOfRecords = await prisma.augments_first_choice_ranking.count({
+        where: { id: id }
+      });
+      if (numOfRecords == 1) {
+        await prisma.augments_first_choice_ranking.update({
+          where: { id: id },
+          data: {
+            sumOfPlacements: {
+              increment: firstChoiceAugmentObject[id]['sumOfPlacement']
+            },
+            sumOfWins: { increment: firstChoiceAugmentObject[id]['winrate'] },
+            numberOfAppearances: {
+              increment: firstChoiceAugmentObject[id]['frequency']
+            }
+          }
+        });
+      } else {
+        await prisma.augments_first_choice_ranking.create({
+          data: {
+            id: id,
+            sumOfPlacements: firstChoiceAugmentObject[id]['sumOfPlacement'],
+            numberOfAppearances: firstChoiceAugmentObject[id]['frequency'],
+            sumOfWins: firstChoiceAugmentObject[id]['winrate']
+          }
+        });
+      }
+    }
+
+    for (const id in secondChoiceAugmentObject) {
+      const numOfRecords = await prisma.augments_second_choice_ranking.count({
+        where: { id: id }
+      });
+      if (numOfRecords == 1) {
+        await prisma.augments_second_choice_ranking.update({
+          where: { id: id },
+          data: {
+            sumOfPlacements: {
+              increment: secondChoiceAugmentObject[id]['sumOfPlacement']
+            },
+            sumOfWins: { increment: secondChoiceAugmentObject[id]['winrate'] },
+            numberOfAppearances: {
+              increment: secondChoiceAugmentObject[id]['frequency']
+            }
+          }
+        });
+      } else {
+        await prisma.augments_second_choice_ranking.create({
+          data: {
+            id: id,
+            sumOfPlacements: secondChoiceAugmentObject[id]['sumOfPlacement'],
+            numberOfAppearances: secondChoiceAugmentObject[id]['frequency'],
+            sumOfWins: secondChoiceAugmentObject[id]['winrate']
+          }
+        });
+      }
+    }
+
+    for (const id in thirdChoiceAugmentObject) {
+      const numOfRecords = await prisma.augments_third_choice_ranking.count({
+        where: { id: id }
+      });
+      if (numOfRecords == 1) {
+        await prisma.augments_third_choice_ranking.update({
+          where: { id: id },
+          data: {
+            sumOfPlacements: {
+              increment: thirdChoiceAugmentObject[id]['sumOfPlacement']
+            },
+            sumOfWins: { increment: thirdChoiceAugmentObject[id]['winrate'] },
+            numberOfAppearances: {
+              increment: thirdChoiceAugmentObject[id]['frequency']
+            }
+          }
+        });
+      } else {
+        await prisma.augments_third_choice_ranking.create({
+          data: {
+            id: id,
+            sumOfPlacements: thirdChoiceAugmentObject[id]['sumOfPlacement'],
+            numberOfAppearances: thirdChoiceAugmentObject[id]['frequency'],
+            sumOfWins: thirdChoiceAugmentObject[id]['winrate']
           }
         });
       }

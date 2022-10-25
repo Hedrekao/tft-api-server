2af7fea7dcd8a53dc78016ca29bb18c12f18f3ba
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2022-10-25 09:05:20 +0200

    Fix to sorting items in analysis

--- dist/helper_functions/analyzeRoute/analyzeItems.js
+++ dist/helper_functions/analyzeRoute/analyzeItems.js
@@ -13,17 +13,17 @@ const analyzeItems = (inputData, itemsData, numberOfMatchingComps) => {
             analyzedItems.push(analyzedItem);
         }
         analyzedItems.sort((a, b) => {
-            if (parseFloat(a['avgPlace']) > parseFloat(b['playRate'])) {
+            if (parseFloat(a['avgPlace']) > parseFloat(b['avgPlace'])) {
                 return -1;
             }
-            else if (parseFloat(a['playRate']) < parseFloat(b['playRate'])) {
+            else if (parseFloat(a['avgPlace']) < parseFloat(b['avgPlace'])) {
                 return 1;
             }
             else {
-                if (parseFloat(a['avgPlace']) < parseFloat(b['avgPlace'])) {
+                if (parseFloat(a['playRate']) < parseFloat(b['playRate'])) {
                     return -1;
                 }
-                else if (parseFloat(a['avgPlace']) > parseFloat(b['avgPlace'])) {
+                else if (parseFloat(a['playRate']) > parseFloat(b['playRate'])) {
                     return 1;
                 }
             }

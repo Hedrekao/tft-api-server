59f3b025c8d95a6e0f8c72479e68aa24a383fbe2
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2022-12-22 14:41:02 +0100

    map augments function

---
+++ helper_functions/summonerRoute/mapAugments.ts
@@ -0,0 +1,23 @@
+import { cache } from '../singletonCache.js';
+
+const mapAugments = (augments: Array<string>) => {
+  const dataDragon: DataDragon | undefined = cache.get('dataDragon');
+  const set8Data = dataDragon?.items;
+  const result = [];
+
+  for (let i = 0; i < augments.length; i++) {
+    const dataDragonItem = set8Data![augments[i]];
+    const iconWithWrongExt = dataDragonItem?.icon.toLowerCase();
+    const icon = iconWithWrongExt
+      ?.substring(0, iconWithWrongExt.length - 3)
+      .concat('png');
+    result.push({
+      apiName: augments[i],
+      name: dataDragonItem?.name,
+      icon: `https://raw.communitydragon.org/latest/game/${icon}`
+    });
+  }
+  return result;
+};
+
+export default mapAugments;

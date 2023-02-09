436884d9d70d76970081a979346003a2654915c9
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2023-02-09 13:43:39 +0100

    clear database function

---
+++ helper_functions/tasks/clearDatabase.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function clearDatabase() {
+  await prisma.general_data.deleteMany();
+  await prisma.augments_ranking.deleteMany();
+  await prisma.augments_first_choice_ranking.deleteMany();
+  await prisma.augments_second_choice_ranking.deleteMany();
+  await prisma.augments_third_choice_ranking.deleteMany();
+  await prisma.items_ranking.deleteMany();
+  await prisma.champions_ranking.deleteMany();
+}
